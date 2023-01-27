7c40bbc2453922a1958728e1391ba5ceb0d5e8ad
Frah01 <vinario.francesco@gmail.com>
2023-01-27 13:30:48 +0100

    Esercizio completo

---
+++ badword.php
@@ -0,0 +1,29 @@
+<?php 
+
+$paragrafo = $_POST['paragrafo'];
+$nuovo_paragrafo = str_replace('Lorem','***', $paragrafo);
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        <?php echo $paragrafo ?>
+        <?php echo strlen ($paragrafo) ?>
+    </h1>
+    <h2>
+        <?php echo $nuovo_paragrafo ?>
+        <?php echo strlen ($nuovo_paragrafo) ?>
+    </h2>
+        
+    
+</body>
+</html>
